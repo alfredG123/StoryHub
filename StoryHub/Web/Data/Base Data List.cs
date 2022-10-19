f26b231c881df1d5df7bd13f2ed6595cca1ded88
alfredG123 <game501294163@gmail.com>
2022-10-18 21:32:21 -0400

    Created a new object for the character data list.

---
+++ StoryHub/Web/Data/Base Data List.cs
@@ -0,0 +1,33 @@
+﻿using Web.Data.ID;
+
+namespace Web.Data
+{
+    [Serializable()]
+    public abstract class BaseDataList<TID, TData>
+    : List<TData>
+           where TID : BaseID
+        where TData : BaseDatabaseData
+    {
+        /// <summary>
+        /// Return the data by the specified ID
+        /// </summary>
+        /// <param name="data_id"></param>
+        /// <returns></returns>
+        public abstract TData? GetListItem(TID data_id);
+
+        /// <summary>
+        /// Retrieve all data from database
+        /// </summary>
+        /// <param name="db_context"></param>
+        protected abstract void RetrieveData(ProgramDbContext db_context);
+
+        /// <summary>
+        /// Retrieve all data from the database
+        /// </summary>
+        /// <param name="db_context"></param>
+        public BaseDataList(ProgramDbContext db_context)
+        {
+            RetrieveData(db_context);
+        }
+    }
+}
---
+++ StoryHub/Web/Data/Characters/List/Character Data List.cs
@@ -0,0 +1,44 @@
+﻿using Web.Data.ID;
+using Web.Models;
+
+namespace Web.Data.Characters
+{
+    [Serializable()]
+    public class CharacterDataList
+    : BaseDataList<CharacterID, CharacterData>
+    {
+        /// <summary>
+        /// Retrieve all character data from the database
+        /// </summary>
+        /// <param name="db_context"></param>
+        public CharacterDataList(ProgramDbContext db_context)
+            : base(db_context)
+        {
+        }
+
+        /// <summary>
+        /// Return the character data by the specified ID
+        /// </summary>
+        /// <param name="character_id"></param>
+        /// <returns></returns>
+        public override CharacterData? GetListItem(CharacterID character_id)
+        {
+            return (this.Where(list_item => list_item.CharacterID == character_id).SingleOrDefault());
+        }
+
+        /// <summary>
+        /// Retrieve all character data from database
+        /// </summary>
+        /// <param name="db_context"></param>
+        protected override void RetrieveData(ProgramDbContext db_context)
+        {
+            // NOTE: Close the connection first by using ToList() instead iterating db_context.CharacterData
+            List<CharacterModel> character_data_list = db_context.CharacterData.ToList();
+
+            foreach (CharacterModel character_model in character_data_list)
+            {
+                this.Add(new CharacterData(character_model));
+            }
+        }
+    }
+}

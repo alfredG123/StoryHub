dd861ddf7ea5eef6244ea0a65e8e1916d8499dd0
alfredG123 <game501294163@gmail.com>
2022-10-26 23:54:43 -0400

    Created new classes for the plot region links.

--- StoryHub/Web/Data/Plots/Links/Plot Region Links.cs
+++ StoryHub/Web/Data/Plots/Links/Plot Region Links.cs
@@ -1,7 +1,104 @@
-﻿namespace Web.Data.Plots
+﻿using Web.Data.ID;
+using Web.Models;
+
+namespace Web.Data.Plots
 {
     [Serializable()]
     public class PlotRegionLinks
+        : BaseLinks<PlotID, RegionID>
     {
+        /// <summary>
+        /// Retrieve all plot region link items from database
+        /// </summary>
+        /// <param name="link_type"></param>
+        /// <param name="id"></param>
+        /// <param name="db_context"></param>
+        public PlotRegionLinks(LinkType link_type, BaseID id, ProgramDbContext db_context)
+            : base(link_type, id, db_context)
+        {
+        }
+
+        /// <summary>
+        /// Retrieve all link items from database
+        /// </summary>
+        /// <param name="link_type"></param>
+        /// <param name="id"></param>
+        /// <param name="db_context"></param>
+        protected override void RetrieveLinks(LinkType link_type, BaseID id, ProgramDbContext db_context)
+        {
+            // NOTE: Close the connection first by using ToList() instead iterating db_context.PlotRegionLinkItemModel
+            List<PlotRegionLinkItemModel> plot_region_link_item_model_list;
+
+            if (link_type == LinkType.ByLeft)
+            {
+                plot_region_link_item_model_list = db_context.PlotRegionLinkItem.Where(list_item => list_item.PlotID == id.Value).ToList();
+            }
+            else
+            {
+                plot_region_link_item_model_list = db_context.PlotRegionLinkItem.Where(list_item => list_item.RegionID == id.Value).ToList();
+            }
+
+            foreach (PlotRegionLinkItemModel plot_region_link_item_model in plot_region_link_item_model_list)
+            {
+                this.Add(new PlotRegionLinkItem(plot_region_link_item_model));
+            }
+        }
+    }
+
+    [Serializable()]
+    public class PlotRegionLinkItem
+         : BaseLinkItem<PlotID, RegionID>
+    {
+        /// <summary>
+        /// Load the plot region link item model
+        /// </summary>
+        /// <param name="plot_region_link_item_model"></param>
+        public PlotRegionLinkItem(PlotRegionLinkItemModel plot_region_link_item_model)
+            : base(new PlotRegionID(plot_region_link_item_model.ID), plot_region_link_item_model)
+        {
+        }
+
+        /// <summary>
+        /// Return or set the plot ID
+        /// </summary>
+        public PlotID PlotID { get; set; } = new();
+
+        /// <summary>
+        /// Return or set the region ID
+        /// </summary>
+        public RegionID RegionID { get; set; } = new();
+
+        /// <summary>
+        /// Return or set the description
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Update the data object for retrieving the data from the database
+        /// </summary>
+        protected override void UpdateDataObject()
+        {
+            PlotRegionLinkItemModel plot_region_link_item_model = (PlotRegionLinkItemModel)this.BaseModel;
+            PlotRegionID plot_region_id = new(plot_region_link_item_model.ID);
+
+            this.ID = plot_region_id;
+            this.PlotID = new PlotID(plot_region_link_item_model.PlotID);
+            this.RegionID = new RegionID(plot_region_link_item_model.RegionID);
+            this.Description = plot_region_link_item_model.Description;
+            this.IsSet = plot_region_id.IsSet;
+        }
+
+        /// <summary>
+        /// Update the model object for saving the data in database
+        /// </summary>
+        protected override void UpdateModelObject()
+        {
+            PlotRegionLinkItemModel plot_region_link_item_model = (PlotRegionLinkItemModel)this.BaseModel;
+
+            plot_region_link_item_model.ID = this.ID.Value;
+            plot_region_link_item_model.PlotID = this.PlotID.Value;
+            plot_region_link_item_model.RegionID = this.RegionID.Value;
+            plot_region_link_item_model.Description = this.Description;
+        }
     }
 }

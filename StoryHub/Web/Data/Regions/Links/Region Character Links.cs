c908b3a3383aa3b5e1ff29b2fde2ad25bf12dd82
alfredG123 <game501294163@gmail.com>
2022-10-27 23:55:50 -0400

    Created new classes for the region character links.

--- StoryHub/Web/Data/Regions/Links/Region Character Links.cs
+++ StoryHub/Web/Data/Regions/Links/Region Character Links.cs
@@ -1,7 +1,97 @@
-﻿namespace Web.Data.Regions
+﻿using Web.Data.ID;
+using Web.Models;
+
+namespace Web.Data.Plots
 {
     [Serializable()]
     public class RegionCharacterLinks
+        : BaseLinks<RegionID, CharacterID>
     {
+        /// <summary>
+        /// Retrieve all region character link items from database
+        /// </summary>
+        /// <param name="link_type"></param>
+        /// <param name="id"></param>
+        /// <param name="db_context"></param>
+        public RegionCharacterLinks(LinkType link_type, BaseID id, ProgramDbContext db_context)
+            : base(link_type, id, db_context)
+        {
+        }
+
+        /// <summary>
+        /// Retrieve all link items from database
+        /// </summary>
+        /// <param name="link_type"></param>
+        /// <param name="id"></param>
+        /// <param name="db_context"></param>
+        protected override void RetrieveLinks(LinkType link_type, BaseID id, ProgramDbContext db_context)
+        {
+            // NOTE: Close the connection first by using ToList() instead iterating db_context.RegionCharacterLinkItemModel
+            List<RegionCharacterLinkItemModel> region_character_link_item_model_list;
+
+            if (link_type == LinkType.ByLeft)
+            {
+                region_character_link_item_model_list = db_context.RegionCharacterLinkItem.Where(list_item => list_item.RegionID == id.Value).ToList();
+            }
+            else
+            {
+                region_character_link_item_model_list = db_context.RegionCharacterLinkItem.Where(list_item => list_item.CharacterID == id.Value).ToList();
+            }
+
+            foreach (RegionCharacterLinkItemModel region_character_link_item_model in region_character_link_item_model_list)
+            {
+                this.Add(new RegionCharacterLinkItem(region_character_link_item_model));
+            }
+        }
+    }
+
+    [Serializable()]
+    public class RegionCharacterLinkItem
+         : BaseLinkItem<RegionID, CharacterID>
+    {
+        /// <summary>
+        /// Load the region character link item model
+        /// </summary>
+        /// <param name="region_character_link_item_model"></param>
+        public RegionCharacterLinkItem(RegionCharacterLinkItemModel region_character_link_item_model)
+            : base(new RegionCharacterID(region_character_link_item_model.ID), region_character_link_item_model)
+        {
+        }
+
+        /// <summary>
+        /// Return or set the region ID
+        /// </summary>
+        public RegionID RegionID { get; set; } = new();
+
+        /// <summary>
+        /// Return or set the character ID
+        /// </summary>
+        public CharacterID CharacterID { get; set; } = new();
+
+        /// <summary>
+        /// Update the data object for retrieving the data from the database
+        /// </summary>
+        protected override void UpdateDataObject()
+        {
+            RegionCharacterLinkItemModel region_character_link_item_model = (RegionCharacterLinkItemModel)this.BaseModel;
+            RegionCharacterID region_character_id = new(region_character_link_item_model.ID);
+
+            this.ID = region_character_id;
+            this.RegionID = new RegionID(region_character_link_item_model.RegionID);
+            this.CharacterID = new CharacterID(region_character_link_item_model.CharacterID);
+            this.IsSet = region_character_id.IsSet;
+        }
+
+        /// <summary>
+        /// Update the model object for saving the data in database
+        /// </summary>
+        protected override void UpdateModelObject()
+        {
+            RegionCharacterLinkItemModel region_character_link_item_model = (RegionCharacterLinkItemModel)this.BaseModel;
+
+            region_character_link_item_model.ID = this.ID.Value;
+            region_character_link_item_model.RegionID = this.RegionID.Value;
+            region_character_link_item_model.CharacterID = this.CharacterID.Value;
+        }
     }
 }

dd24f74cbf1b773a0dd1a5be7b3b893a4357c324
alfredG123 <game501294163@gmail.com>
2022-10-30 23:28:06 -0400

    Create a page for the story list.

---
+++ StoryHub/Web/Controllers/Story Controller.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Data;
+using Web.Data.Stories;
+using Web.Miscellaneous;
+using X.PagedList;
+
+namespace Web.Controllers
+{
+    public class StoryController : Controller
+    {
+        private readonly ProgramDbContext _db_context;
+        
+        // Paging variables
+        private static int _page_number_for_story_data_list_page = 1;
+
+        public StoryController(ProgramDbContext db_context)
+        {
+            _db_context = db_context;
+        }
+
+        #region "Story List"
+        public IActionResult Index(int? page_number)
+        {
+            StoryDataList? story_data_list = null;
+
+            // If the story data list is not retrieve yet, load all the story data from the database
+            if (story_data_list == null)
+            {
+                // Load all the story data from the database
+                story_data_list = new(_db_context);
+            }
+
+            // If the page number is not specified, use the last stored page number
+            if (page_number == null)
+            {
+                page_number = _page_number_for_story_data_list_page;
+            }
+
+            // Adjust the page number to fit the list, and store the page number in case the page refresh due to actions such as deletion
+            _page_number_for_story_data_list_page = GlobalMethods.GetValidPageNumber(page_number, story_data_list.Count);
+
+            return View("StoryList", story_data_list.ToPagedList(_page_number_for_story_data_list_page, GlobalMethods.PAGE_SIZE));
+        }
+        #endregion
+    }
+}
---
+++ StoryHub/Web/Miscellaneous/Global Methods/Global Methods.cs
@@ -0,0 +1,94 @@
+﻿namespace Web.Miscellaneous
+{
+    public static class GlobalMethods
+    {
+        public const int PAGE_SIZE = 8;
+
+        /// <summary>
+        /// Return the page number
+        /// If the page number is not specified, return the first page
+        /// </summary>
+        /// <param name="page_number"></param>
+        /// <param name="total_item_count"></param>
+        /// <returns></returns>
+        public static int GetValidPageNumber(int? page_number, int total_item_count)
+        {
+            // Set up the default value for the page number
+            int valid_page_number = GetPageNumberForFirstPage();
+
+            // If the page number is specified, update return value
+            if ((page_number != null) && (page_number > 0))
+            {
+                if (total_item_count > 0)
+                {
+                    // Calculate the items on the last page
+                    int reminder = total_item_count % PAGE_SIZE;
+
+                    // Calculate the maximum number of the possible pages
+                    int max_number_page = (total_item_count - reminder) / PAGE_SIZE;
+                    if (reminder > 0)
+                    {
+                        max_number_page += 1;
+                    }
+
+                    // If the page number is greater than the maximum number of the possible pages, update the page number
+                    // Otherwise, keep the page number
+                    valid_page_number = Math.Min(max_number_page, (int)page_number);
+                }
+            }
+
+            return (valid_page_number);
+        }
+
+        /// <summary>
+        /// Return the page number for the first page
+        /// </summary>
+        /// <returns></returns>
+        public static int GetPageNumberForFirstPage()
+        {
+            return (1);
+        }
+
+        /// <summary>
+        /// Return the paging text, X of Y
+        /// </summary>
+        /// <param name="page_number"></param>
+        /// <param name="page_count"></param>
+        /// <returns></returns>
+        public static string GetPagingText(int? page_number, int? page_count)
+        {
+            // Set the default values
+            page_number ??= 1;
+            page_count ??= 1;
+
+            // Build the text for the paging info
+            string paging_text = "Page " + page_number.ToString() + " of " + page_count.ToString();
+
+            return (paging_text);
+        }
+
+        /// <summary>
+        /// Return the row number
+        /// </summary>
+        /// <param name="current_row_number"></param>
+        /// <param name="page_number"></param>
+        /// <returns></returns>
+        public static int GetRowNumberForPageNumber(int? current_row_number, int? page_number)
+        {
+            // Set up default values
+            current_row_number ??= 1;
+            page_number ??= 1;
+
+            // Throw the error, if the row number is invalid
+            if (current_row_number > PAGE_SIZE)
+            {
+                throw new ArgumentOutOfRangeException(nameof(current_row_number), "Row number is greater than the page size.");
+            }
+
+            // Calculate the row number
+            int row_number = (((int)page_number) - 1) * PAGE_SIZE + (int)current_row_number;
+
+            return (row_number);
+        }
+    }
+}

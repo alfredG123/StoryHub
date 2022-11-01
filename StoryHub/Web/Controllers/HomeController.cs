6533918ab18b673d40c523d9ead9723795eef701
alfredG123 <game501294163@gmail.com>
2022-10-31 23:32:20 -0400

    Reworked the style of the pages.

--- StoryHub/Web/Controllers/HomeController.cs
+++ StoryHub/Web/Controllers/HomeController.cs
@@ -6,11 +6,8 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
         }
 
         public IActionResult Index()

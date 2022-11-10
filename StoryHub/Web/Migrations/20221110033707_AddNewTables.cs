90bc33ac9187bb7ceffac701363656aaf89553ce
alfredG123 <game501294163@gmail.com>
2022-11-09 22:38:03 -0500

    Modified the database.

---
+++ StoryHub/Web/Migrations/20221110033707_AddNewTables.cs
@@ -0,0 +1,294 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Migrations
+{
+    public partial class AddNewTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CharacterData",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterData", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CharacterReferenceLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CharacterID = table.Column<int>(type: "int", nullable: false),
+                    ReferenceID = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterReferenceLinkItem", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomFieldData",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FieldName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomFieldData", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlotCharacterLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlotID = table.Column<int>(type: "int", nullable: false),
+                    CharacterID = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlotCharacterLinkItem", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlotData",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlotType = table.Column<int>(type: "int", nullable: false),
+                    DramaType = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Goal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Scene = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlotData", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlotPlotLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlotID = table.Column<int>(type: "int", nullable: false),
+                    SubPlotID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlotPlotLinkItem", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlotRegionLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlotID = table.Column<int>(type: "int", nullable: false),
+                    RegionID = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlotRegionLinkItem", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReferenceData",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReferenceData", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegionCharacterLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegionID = table.Column<int>(type: "int", nullable: false),
+                    CharacterID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegionCharacterLinkItem", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegionData",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegionData", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegionReferenceLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegionID = table.Column<int>(type: "int", nullable: false),
+                    ReferenceID = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegionReferenceLinkItem", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegionRegionLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegionID = table.Column<int>(type: "int", nullable: false),
+                    SubRegionID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegionRegionLinkItem", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryCharacterLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StoryID = table.Column<int>(type: "int", nullable: false),
+                    CharacterID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryCharacterLinkItem", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryCustomFieldLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StoryID = table.Column<int>(type: "int", nullable: false),
+                    CustomFieldID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryCustomFieldLinkItem", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryPlotLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StoryID = table.Column<int>(type: "int", nullable: false),
+                    PlotID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryPlotLinkItem", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryRegionLinkItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StoryID = table.Column<int>(type: "int", nullable: false),
+                    RegionID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryRegionLinkItem", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CharacterData");
+
+            migrationBuilder.DropTable(
+                name: "CharacterReferenceLinkItem");
+
+            migrationBuilder.DropTable(
+                name: "CustomFieldData");
+
+            migrationBuilder.DropTable(
+                name: "PlotCharacterLinkItem");
+
+            migrationBuilder.DropTable(
+                name: "PlotData");
+
+            migrationBuilder.DropTable(
+                name: "PlotPlotLinkItem");
+
+            migrationBuilder.DropTable(
+                name: "PlotRegionLinkItem");
+
+            migrationBuilder.DropTable(
+                name: "ReferenceData");
+
+            migrationBuilder.DropTable(
+                name: "RegionCharacterLinkItem");
+
+            migrationBuilder.DropTable(
+                name: "RegionData");
+
+            migrationBuilder.DropTable(
+                name: "RegionReferenceLinkItem");
+
+            migrationBuilder.DropTable(
+                name: "RegionRegionLinkItem");
+
+            migrationBuilder.DropTable(
+                name: "StoryCharacterLinkItem");
+
+            migrationBuilder.DropTable(
+                name: "StoryCustomFieldLinkItem");
+
+            migrationBuilder.DropTable(
+                name: "StoryPlotLinkItem");
+
+            migrationBuilder.DropTable(
+                name: "StoryRegionLinkItem");
+        }
+    }
+}

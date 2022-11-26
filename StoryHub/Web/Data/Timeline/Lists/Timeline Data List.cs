91d19b1f678e0a0a2949a262d28fae3d3bca240b
alfredG123 <game501294163@gmail.com>
2022-11-25 23:07:25 -0500

    Modified the timeline list to retrieve by the story.

--- StoryHub/Web/Data/Timeline/Lists/Timeline Data List.cs
+++ StoryHub/Web/Data/Timeline/Lists/Timeline Data List.cs
@@ -1,19 +1,25 @@
 ﻿using Web.Data.ID;
+using Web.Data.Stories;
 using Web.Models;
 
 namespace Web.Data.Timeline
 {
     [Serializable()]
-    public class TimelineDataLine
+    public class TimelineDataList
     : BaseDataList<TimelineID, TimelineData>
     {
+        private readonly StoryID _story_id = new();
+
         /// <summary>
-        /// Retrieve all timeline data from the database
+        /// Retrieve all timeline data from the database for the story
         /// </summary>
+        /// <param name="story_id"></param>
         /// <param name="db_context"></param>
-        public TimelineDataLine(ProgramDbContext db_context)
+        public TimelineDataList(StoryID story_id, ProgramDbContext db_context)
             : base()
         {
+            _story_id = story_id;
+
             RetrieveData(db_context);
         }
 
@@ -36,9 +42,17 @@ namespace Web.Data.Timeline
             // NOTE: Close the connection first by using ToList() instead iterating db_context.TimelineData
             List<TimelineDataModel> timeline_data_model_list = db_context.TimelineData.ToList();
 
-            foreach (TimelineDataModel timeline_data_model in timeline_data_model_list)
+            if (_story_id.IsSet)
             {
-                this.Add(new TimelineData(timeline_data_model));
+                StoryTimelineLinks story_timeline_links = new(_story_id, db_context);
+
+                foreach (TimelineDataModel timeline_data_model in timeline_data_model_list)
+                {
+                    if (story_timeline_links.IndexOfID(new TimelineID(timeline_data_model.ID)) != -1)
+                    {
+                        this.Add(new TimelineData(timeline_data_model));
+                    }
+                }
             }
         }
     }
